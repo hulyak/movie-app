48be19c0f33589a92b15d091733014d641fa1124
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-26 16:48:48 -0700

    feat:add the children of Grid component

--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import {StyledMovieThumb} from '../styles/StyledMovieThumb';
 
-const MovieThumb = () => <div>MovieThumb</div>;
 
+const MovieThumb = ({image, movieId, clickable}) => (
+    <StyledMovieThumb>
+    {clickable ? (
+        <img className="clickable" src={image} alt="moviethumsb" /> 
+    ): (
+      <img src={image} alt="moviethumb" />  //not clickable image
+    )}
+    </StyledMovieThumb>
+)
 export default MovieThumb;
