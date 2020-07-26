6ef397035df91a14615fdfa03ef277d22cfaec81
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-26 16:48:02 -0700

    feat: create grid and show up on the screen

--- src/components/elements/Grid.js
+++ src/components/elements/Grid.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import {StyledGrid, StyledGridContent} from '../styles/StyledGrid';
 
-const Grid = () => <div>Grid</div>;
+//children is MovieThumb
+const Grid = ({header, children}) => (
+  <StyledGrid>
+  <h1>{header}</h1>
+  <StyledGridContent>{children}</StyledGridContent>
+  </StyledGrid>
+
+)
 
 export default Grid;
+
+
